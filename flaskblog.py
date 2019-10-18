a5c880dc82ee5232e3d678bc1d330fa0244c9a65
Shaan <Shaan@admins-MacBook-Pro.local>
2019-10-18 03:46:14 -0700

    adding forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,7 @@
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1fee76297951653b6ae6b3f35f4284bd'
 
 posts = [
     {
@@ -27,11 +28,31 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!, success').format(form.username.data)
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)   
+
 @app.route('/music')
 def music():
     return render_template('music.html')
 
-    
+
 @app.route('/wtn')
 def wtn():
     artist = request.form.get('artist')
